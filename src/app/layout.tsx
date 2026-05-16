82dab39df25d6fc8b204a0d9559786098439e220
CristianFriedrichs <cristian@hubhotelaria.com>
2026-05-16 14:22:09 -0300

    feat: integrate Google Tag Manager (GTM-5BRQQ54Z)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from '@/lib/utils';
 import { validateEnv } from '@/lib/env';
 import { AuthProvider } from '@/lib/auth/AuthContext';
+
+const GTM_ID = 'GTM-5BRQQ54Z';
 
 // Validate env vars at startup
 validateEnv();
@@ -33,7 +36,24 @@ export default function RootLayout({
       className={cn(inter.variable)}
       suppressHydrationWarning
     >
+      <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body className="antialiased" suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <AuthProvider>
           {children}
           <Toaster />
