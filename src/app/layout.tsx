4004755105d7f5fe2162286e2e00351a241fb4fe
CristianFriedrichs <cristian@hubhotelaria.com>
2026-05-16 16:09:25 -0300

    fix: adjust Meta Pixel to exactly match official snippet in <head>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,8 +49,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
-        <Script id="fb-pixel-init" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s)
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -59,8 +61,15 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '${FB_PIXEL_ID}');
-fbq('track', 'PageView');`}
-        </Script>
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1" />`,
+          }}
+        />
+        {/* End Meta Pixel Code */}
       </head>
       <body className="antialiased" suppressHydrationWarning>
         {/* Google Tag Manager (noscript) */}
@@ -70,14 +79,6 @@ fbq('track', 'PageView');`}
           }}
         />
         {/* End Google Tag Manager (noscript) */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-          />
-        </noscript>
         <AuthProvider>
           {children}
           <Toaster />
